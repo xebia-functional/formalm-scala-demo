188fefdc6d9acc5a5e425f19d65795802cb9e520
Alejandro Serrano <trupill@gmail.com>
2023-05-08 14:25:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Demo Scala + SBT
+
+This repository contains a small project that can be analyzed using a variety of formal methods tools and static analyses.
+
+```bash
+sbt compile
+```
---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,16 @@
+package example
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
+
+object Example {
+  def ambrosia(n: Int): Int = {
+    require(n >= 0)
+    n + 1
+  } ensuring { res => res >= n }
+
+  def ejemplo: Int = {
+    ambrosia(-1)
+  }
+}
